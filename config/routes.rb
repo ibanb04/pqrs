7610f179f6491a90845faa3129ab239278876421
Jesus Ramos <fotoramosltda1234@gmail.com>
2018-05-27 18:52:13 -0500

    Se arreglo todos los index con las llaves foraneas

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   resources :permission_roles
   resources :roles
   resources :permissions
+  resources :users
   root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
